e3605bc26d171cfefba8fe6974508e8774a19257
Martial DAMBIA <dambiamartial@gmail.com>
2021-05-19 11:14:51 +0100

    Définition des entités et des interfaces de base: - Création de la classe Entité de base - Création du root aggregate - Création de l'interface repository - Création de l'interface UnitOfWork - Création de la classe d'énumération de base - Création de la classe ValueObjet de base

---
+++ Sigma.Systems.SharedKernel/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigma.Systems.SharedKernel.Interfaces
+{
+    /// <summary>
+    /// Interface de marqueur vide, utilisée pour indiquer que la classe d'entité qui l'implémente est une racine d'agrégat
+    /// </summary>
+    public interface IAggregateRoot
+    {
+    }
+}
